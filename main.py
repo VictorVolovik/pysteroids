97ec4a7a50a0ba23796cebf7381061ec0e87bc03
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-10-09 18:40:09 +0400

    Fix formatting

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 
+
 def main():
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  clock = pygame.time.Clock()
-  dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-  updatable = pygame.sprite.Group()
-  drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-  Player.containers = (updatable, drawable)
-  Player(
-    SCREEN_WIDTH / 2,
-    SCREEN_HEIGHT / 2
-  );
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        dt = clock.tick(60) / 1000
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          return
-    screen.fill("black")
-    dt = clock.tick(60) / 1000
+        for sprite in updatable:
+            sprite.update(dt)
 
-    for sprite in updatable:
-      sprite.update(dt)
-    
-    for sprite in drawable:
-      sprite.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
 
-    pygame.display.flip()
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
